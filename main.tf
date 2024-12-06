44201fbbaea3ebd42f49ac4f35ec570cb459ffb9
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2024-12-06 08:24:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,12 @@ region = "us-east-1"
 resource "aws_instance" "myec2-cloud" {
 
 ami = "ami-0166fe664262f664c"
-instance_type = "t2.large"
+instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance1"
+  }
+
 
 
 }
